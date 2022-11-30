404938798f987cf26709283b91cd67f9ecadac2f
han0224 <dbn0224@gmail.com>
2022-11-30 14:35:27 +0900

    Refactor: 재시작 기능 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const BaseballGame = require("./BaseballGame");
-const BaseballNumber = require("./BaseballNumber");
 const {
   GAME_STATE_MESSAGE,
   GAME_RESULT_MESSAGE,
@@ -27,62 +26,18 @@ class App {
       const result = this.#game.compareUserAndComputer(ball);
       if (result.isEnd) break;
     }
-    console.log("end");
+    console.log("ENDEND");
+    runGenerator(this.isReStart.bind(this));
   }
-
-  /*********/
-  // userInputNumber() {
-  //   MissionUtils.Console.readLine(GAME_STATE_MESSAGE.ENTER_NUMBER, (number) => {
-  //     this.NumberUtil.isBaseballNumber(number);
-  //     const result = this.compareNumber(number);
-  //     MissionUtils.Console.print(result);
-  //     if (result !== "3스트라이크") {
-  //       this.userInputNumber();
-  //     }
-  //     if (result === "3스트라이크") {
-  //       this.isReStart();
-  //     }
-  //   });
-  // }
-
-  // compareNumber(userNumber) {
-  //   const userNumberArray = this.NumberUtil.NumberToArray(userNumber);
-  //   const computerNumberArray = this.NumberUtil.NumberToArray(
-  //     this.computerNumber
-  //   );
-  //   let ball = 0;
-  //   let strike = 0;
-  //   const total =
-  //     6 - new Set([...userNumberArray, ...computerNumberArray]).size;
-
-  //   if (total === 0) return GAME_RESULT_MESSAGE.NOTHING;
-
-  //   for (let index = 0; index < 3; index++) {
-  //     if (userNumberArray[index] === computerNumberArray[index]) {
-  //       strike += 1;
-  //     }
-  //   }
-  //   ball = total - strike;
-  //   if (ball === 0) return `${strike}${GAME_RESULT_MESSAGE.STRIKE}`;
-  //   if (strike === 0) return `${ball}${GAME_RESULT_MESSAGE.BALL}`;
-  //   return `${ball}${GAME_RESULT_MESSAGE.BALL} ${strike}${GAME_RESULT_MESSAGE.STRIKE}`;
-  // }
-
-  // isReStart() {
-  //   MissionUtils.Console.readLine(GAME_STATE_MESSAGE.WIN, (number) => {
-  //     number = Number(number);
-  //     if (number === 1) this.game();
-  //     else if (number === 2) {
-  //       MissionUtils.Console.print(GAME_STATE_MESSAGE.END);
-  //       MissionUtils.Console.close();
-  //     } else throw ERROR_MESSAGE.ONLY_ONE_TWO;
-  //   });
-  // }
-
-  // game() {
-  //   this.computerNumber = this.NumberUtil.getRandomBaseballNumber();
-  //   this.userInputNumber();
-  // }
+  *isReStart() {
+    const command = yield InputView.inputRetry;
+    console.log(command, typeof command);
+    if (command !== 1 && command !== 2) throw ERROR_MESSAGE.ONLY_ONE_TWO;
+    if (command === 1) runGenerator(this.run.bind(this));
+    if (command === 2) {
+      console.log("end");
+    }
+  }
 }
 
 module.exports = App;
--- src/InputView.js
+++ src/InputView.js
@@ -1,11 +1,16 @@
 const { Console } = require("@woowacourse/mission-utils");
 const { GAME_STATE_MESSAGE } = require("./Constants/Message");
 const MakeBaseball = require("./MakeBaseball");
-
+const NumberUtils = require("./Utils/Number");
 const InputView = {
   inputNumber(callback) {
     Console.readLine(GAME_STATE_MESSAGE.ENTER_NUMBER, (number) => {
       callback(MakeBaseball.userBaseball(number));
+    });
+  },
+  inputRetry(callback) {
+    Console.readLine(GAME_STATE_MESSAGE.WIN, (number) => {
+      callback(NumberUtils.returnNumber(number));
     });
   },
 };
